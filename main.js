4da3c66351efdb29f22ce2d7768ea4f33d36d81e
13inuo0 <13inuo0@github.com>
2025-09-10 12:05:34 +0900

    투두리스트 만들기 예제

---
+++ main.js
@@ -0,0 +1,34 @@
+// window.addEventListener("load", function () {});
+// 모든 이미지 HTML 문서 동영상을 모든 리소스가 로드한 후 실행
+// window.addEventListener("DOMContentLoaded", function () {});
+// HTML문서 완료후 실행 (이미지 영상 기다리지 않음)
+// window.onload = function () {
+//   alert("a");
+// };
+
+window.addEventListener("DOMContentLoaded", function () {
+  const btn = this.document.getElementById("btn");
+  const inputField = this.document.querySelector(".inputField");
+  const addValue = this.document.querySelector("#addValue");
+  const result = this.document.querySelector("#result");
+  const allclear = this.document.querySelector("#allClear");
+  inputField.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const todoValue = addValue.value.trim();
+    // console.log(todoValue);
+    // 입력창에 내용을 입력하지 않았을 때
+    if (todoValue === "") {
+      alert("내용을 입력해주세요!");
+      return; //함수 종료
+    }
+    // li 만들기
+    const list = document.createElement("li");
+    console.log(list);
+    list.innerHTML = `${todoValue} <button class="del-btn">X</button>`;
+    result.appendChild(list);
+    // 입력된 내용 빈 문자열로 만들기
+    addValue.value = "";
+    // 입력한 포커스
+    addValue.focus();
+  });
+});
